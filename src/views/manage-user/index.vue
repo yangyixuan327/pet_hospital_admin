c201ac03d7ef3c5d59eb41ae2b295c318ffa0f27
七尾 <40332644+whitfield-lyx@users.noreply.github.com>
2021-03-22 16:43:54 +0800

    update：修改用户管理界面的表单样式，增加内联修改按钮和提示

--- src/views/manage-user/index.vue
+++ src/views/manage-user/index.vue
@@ -8,35 +8,32 @@
       fit
       highlight-current-row
     >
-      <el-table-column align="center" label="ID" width="95">
+      <el-table-column align="center" label="ID" width="100">
         <template slot-scope="scope">
           {{ scope.$index }}
         </template>
       </el-table-column>
-      <el-table-column label="Title">
+      <el-table-column label="UserName">
         <template slot-scope="scope">
-          {{ scope.row.title }}
+          <span>{{ scope.row.name }}</span>
         </template>
       </el-table-column>
-      <el-table-column label="Author" width="110" align="center">
+      <el-table-column label="Account" width="200" align="center">
         <template slot-scope="scope">
-          <span>{{ scope.row.author }}</span>
+          {{ scope.row.account }}
         </template>
       </el-table-column>
-      <el-table-column label="Pageviews" width="110" align="center">
+      <el-table-column label="Password" width="200" align="center">
         <template slot-scope="scope">
-          {{ scope.row.pageviews }}
+          <el-input v-show="scope.row.edit" size="small" v-model="scope.row.password"></el-input>
+          <span v-show="!scope.row.edit">{{ scope.row.password }}</span>
         </template>
       </el-table-column>
-      <el-table-column class-name="status-col" label="Status" width="110" align="center">
-        <template slot-scope="scope">
-          <el-tag :type="scope.row.status | statusFilter">{{ scope.row.status }}</el-tag>
-        </template>
-      </el-table-column>
-      <el-table-column align="center" prop="created_at" label="Display_time" width="200">
-        <template slot-scope="scope">
-          <i class="el-icon-time" />
-          <span>{{ scope.row.display_time }}</span>
+      <el-table-column align="center" label="Actions" width="400">
+        <template slot-scope="{row}">
+          <el-button v-show='!row.edit' type="primary" @click='cancelEdit(row);row.edit=true;' icon="edit">修改密码</el-button>
+          <el-button v-show='row.edit' type="success" @click='confirmEdit(row);row.edit=false;' icon="check">修改完成</el-button>
+          <el-button type="danger" @click="deleteUser">删除账户</el-button>
         </template>
       </el-table-column>
     </el-table>
@@ -44,36 +41,82 @@
 </template>
 
 <script>
-import { getList } from '@/api/table'
+import { getUserList } from '@/api/user'
 
 export default {
-  filters: {
-    statusFilter(status) {
-      const statusMap = {
-        published: 'success',
-        draft: 'gray',
-        deleted: 'danger'
-      }
-      return statusMap[status]
-    }
-  },
   data() {
     return {
-      list: null,
-      listLoading: true
+      list: [{
+        index: '0',
+        name: '我是超级账号！',
+        account: 'admin',
+        password: '111111',
+        edit: true
+      }],
+      listLoading: false,
+      listQuery: {
+        page: 1,
+        limit: 10
+      }
     }
   },
-  created() {
+  methods: {
+    cancelEdit(row) {
+      row.title = row.originalTitle
+      row.edit = false
+      this.$message({
+        message: 'The title has been restored to the original value',
+        type: 'warning'
+      })
+    },
+    confirmEdit(row) {
+      row.edit = false
+      row.originalTitle = row.title
+      this.$message({
+        message: 'The title has been edited',
+        type: 'success'
+      })
+    },
+    deleteUser(){
+      this.$confirm('此操作将永久删除该账户, 是否继续?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        this.$message({
+          type: 'success',
+          message: '删除成功!'
+        });
+      }).catch(() => {
+        this.$message({
+          type: 'info',
+          message: '已取消删除'
+        });
+      });
+    }
+  }
+  /* created() {
     this.fetchData()
   },
   methods: {
+  async getList() {
+        this.listLoading = true
+        const { data } = await fetchList(this.listQuery)
+        const items = data.items
+        this.list = items.map(v => {
+          this.$set(v, 'edit', false) //
+          v.originalTitle = v.title //  will be used when user click the cancel botton
+          return v
+        })
+        this.listLoading = false
+      },
     fetchData() {
       this.listLoading = true
-      getList().then(response => {
+      getUserList().then(response => {
         this.list = response.data.items
         this.listLoading = false
       })
     }
-  }
+  }*/
 }
 </script>

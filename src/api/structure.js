5341dda517fc37bf94ac39d01d35001733d56f46
zhouxingyu88 <33973737+zhouxingyu88@users.noreply.github.com>
2021-03-24 21:45:53 +0800

    update: complete section_management part

--- src/api/structure.js
+++ src/api/structure.js
@@ -7,3 +7,35 @@ export function getStructureInfo(params) {
     params
   })
 }
+
+export function deleteSectionById(section_id) {
+  const params = {
+    'section_id': section_id
+  }
+  // todo 修改api
+  return request({
+    url: '/vue-admin-template/table/list',
+    method: 'get',
+    params
+  })
+}
+
+export function submitSectionWordsDialogResult(params) {
+  if (params.changeMode != null) {
+    if (params.changeMode === 'add') {
+      // 添加科室
+      return request({
+        url: '/vue-admin-template/table/list',
+        method: 'get',
+        params
+      })
+    } else {
+      // 修改科室
+      return request({
+        url: '/vue-admin-template/table/list',
+        method: 'get',
+        params
+      })
+    }
+  }
+}
--- src/views/manage-structure/index.vue
+++ src/views/manage-structure/index.vue
@@ -9,12 +9,12 @@
       /></el-button>
       <template #dropdown>
         <el-dropdown-menu>
-          <el-dropdown-item command="keShi">科室管理</el-dropdown-item>
-          <el-dropdown-item command="yaoPin">药品管理</el-dropdown-item>
-          <el-dropdown-item command="shouFei">收费管理</el-dropdown-item>
-          <el-dropdown-item command="huaYan">化验项目管理</el-dropdown-item>
-          <el-dropdown-item command="yiMiao">疫苗管理</el-dropdown-item>
-          <el-dropdown-item command="zhuYuan">住院管理</el-dropdown-item>
+          <el-dropdown-item command="section">科室管理</el-dropdown-item>
+          <el-dropdown-item command="medicine">药品管理</el-dropdown-item>
+          <el-dropdown-item command="fee">收费管理</el-dropdown-item>
+          <el-dropdown-item command="examination">化验项目管理</el-dropdown-item>
+          <el-dropdown-item command="vaccine">疫苗管理</el-dropdown-item>
+          <el-dropdown-item command="hospitalize">住院管理</el-dropdown-item>
         </el-dropdown-menu>
       </template>
     </el-dropdown>
@@ -53,7 +53,7 @@
       </el-table-column>
       <el-table-column v-if="tag==='科室管理'" :label="column6" align="center">
         <template slot-scope="scope">
-          <el-button v-if="scope.row.description4==='image'" type="primary" @click="onImageClicked(scope.row.caseId, scope.row.jieZhen)">图片</el-button>
+          <el-button type="primary" @click="onImageClicked(scope.row.caseId, scope.row.jieZhen)">图片</el-button>
         </template>
       </el-table-column>
       <el-table-column align="center" prop="created_at" label="操作" width="200">
@@ -65,11 +65,59 @@
         </template>
       </el-table-column>
     </el-table>
+    <el-dialog
+      :visible="sectionWordsDialog.visible"
+      :title="sectionWordsDialog.title"
+      width="50%"
+      center
+    >
+      <el-form :model="secForm">
+        <el-form-item label="科室名" label-width="120px">
+          <el-input v-model="secForm.sectionName" autocomplete="off"/>
+        </el-form-item>
+        <el-form-item label="前台功能描述" label-width="120px">
+          <el-input v-model="secForm.recDesc" autocomplete="off"/>
+        </el-form-item>
+        <el-form-item label="医助功能描述" label-width="120px">
+          <el-input v-model="secForm.assissDesc" autocomplete="off"/>
+        </el-form-item>
+        <el-form-item label="医师功能描述" label-width="120px">
+          <el-input v-model="secForm.docDesc" autocomplete="off"/>
+        </el-form-item>
+      </el-form>
+      <template #footer>
+        <span class="dialog-footer">
+          <el-button @click="sectionWordsDialog.visible = false">取 消</el-button>
+          <el-button type="primary" @click="sectionWordsDialogConfirmOnClicked">确 定</el-button>
+        </span>
+      </template>
+    </el-dialog>
+    <el-dialog
+      :visible="medicineDialog.visible"
+      :title="medicineDialog.title"
+      width="50%"
+      center
+    >
+      <el-form :model="medForm">
+        <el-form-item label="药品名" label-width="120px">
+          <el-input v-model="medForm.medName" autocomplete="off"/>
+        </el-form-item>
+        <el-form-item label="功能描述" label-width="120px">
+          <el-input v-model="medForm.medDesc" autocomplete="off"/>
+        </el-form-item>
+      </el-form>
+      <template #footer>
+        <span class="dialog-footer">
+          <el-button @click="medicineDialog.visible = false">取 消</el-button>
+          <el-button type="primary" @click="medicineDialogConfirmOnClicked">确 定</el-button>
+        </span>
+      </template>
+    </el-dialog>
   </div>
 </template>
 
 <script>
-import { getStructureInfo } from '@/api/structure'
+import { getStructureInfo, submitSectionWordsDialogResult, deleteSectionById } from '@/api/structure'
 
 export default {
   filters: {
@@ -92,16 +140,153 @@ export default {
       column4: 'column4',
       column5: 'column5',
       column6: 'column6',
-      tag: '选择结构'
+      tag: '选择结构',
+      sectionWordsDialog: {
+        visible: false,
+        title: '',
+        changeMode: 'add'
+      },
+      secForm: {
+        sectionId: -1,
+        sectionIndex: -1,
+        sectionName: '',
+        recDesc: '',
+        assissDesc: '',
+        docDesc: ''
+      },
+      medicineDialog: {
+        visible: false,
+        title: '',
+        changeMode: 'add'
+      },
+      medForm: {
+        medId: -1,
+        medIndex: -1,
+        medName: '',
+        medDesc: ''
+      }
     }
   },
   created() {
-    this.handleCommand('keShi')
+    this.handleCommand('section')
   },
   methods: {
+    onCreateNewClicked() {
+      if (this.tag === '科室管理') {
+        this.sectionWordsDialog.visible = true
+        this.sectionWordsDialog.title = '创建科室'
+        this.secForm.sectionName = ''
+        this.secForm.recDesc = ''
+        this.secForm.assissDesc = ''
+        this.secForm.docDesc = ''
+        this.sectionWordsDialog.changeMode = 'add'
+      } else if (this.tag === '药品管理') {
+        this.medicineDialog.visible = true
+        this.medicineDialog.title = '创建药品'
+        this.medForm.medName = ''
+        this.medForm.medDesc = ''
+        this.medicineDialog.changeMode = 'add'
+      } else if (this.tag === '收费管理') {
+        // add fee
+      } else if (this.tag === '化验项目管理') {
+        // add exam
+      } else if (this.tag === '疫苗管理') {
+        // add vac
+      } else if (this.tag === '住院管理') {
+        // add hos
+      }
+    },
+    onEditClicked(edit_id, edit_index) {
+      if (this.tag === '科室管理') {
+        this.sectionWordsDialog.title = '编辑科室'
+        this.secForm.sectionId = edit_id
+        this.secForm.sectionIndex = edit_index
+        this.secForm.sectionName = this.list[edit_index].name
+        this.secForm.recDesc = this.list[edit_index].description1
+        this.secForm.assissDesc = this.list[edit_index].description2
+        this.secForm.docDesc = this.list[edit_index].description3
+        this.sectionWordsDialog.changeMode = 'update'
+        this.sectionWordsDialog.visible = true
+      } else if (this.tag === '药品管理') {
+        this.medicineDialog.visible = true
+        this.medicineDialog.title = '编辑药品'
+        this.medForm.medId = edit_id
+        this.medForm.medIndex = edit_index
+        this.medForm.medName = this.list[edit_index].name
+        this.medForm.medDesc = this.list[edit_index].description1
+        this.medicineDialog.changeMode = 'update'
+      } else if (this.tag === '收费管理') {
+        // edit fee
+      } else if (this.tag === '化验项目管理') {
+        // edit exam
+      } else if (this.tag === '疫苗管理') {
+        // edit vac
+      } else if (this.tag === '住院管理') {
+        // edit hos
+      }
+    },
+    onDeleteClicked(del_id, del_index) {
+      if (this.tag === '科室管理') {
+        deleteSectionById(del_id).then(response => {
+          // if (response.data.result === 200) {
+          // eslint-disable-next-line no-constant-condition
+          if (true) {
+            console.log(this.list)
+            this.list.splice(del_index, 1)
+          } else {
+            // console.log('删除失败')
+          }
+        })
+      } else if (this.tag === '药品管理') {
+        // delete med
+      } else if (this.tag === '收费管理') {
+        // delete fee
+      } else if (this.tag === '化验项目管理') {
+        // delete exam
+      } else if (this.tag === '疫苗管理') {
+        // delete vac
+      } else if (this.tag === '住院管理') {
+        // delete hos
+      }
+    },
+    sectionWordsDialogConfirmOnClicked() {
+      const params = {
+        sectionId: this.secForm.sectionId,
+        sectionIndex: this.secForm.sectionIndex,
+        sectionName: this.secForm.sectionName,
+        recDesc: this.secForm.recDesc,
+        docDesc: this.secForm.docDesc,
+        assissDesc: this.secForm.assissDesc,
+        changeMode: this.sectionWordsDialog.changeMode
+      }
+      submitSectionWordsDialogResult(params).then(response => {
+        const sectionIndex = this.secForm.sectionIndex
+        const changeMode = this.sectionWordsDialog.changeMode
+        if (changeMode === 'update') {
+          if (sectionIndex != null && sectionIndex >= 0) {
+            this.list[sectionIndex].name = this.secForm.sectionName
+            this.list[sectionIndex].description1 = this.secForm.recDesc
+            this.list[sectionIndex].description3 = this.secForm.docDesc
+            this.list[sectionIndex].description2 = this.secForm.assissDesc
+          }
+        } else if (changeMode === 'add') {
+          this.list.push(
+            {
+              id: 55,
+              name: this.secForm.sectionName,
+              description1: this.secForm.recDesc,
+              description2: this.secForm.assissDesc,
+              description3: this.secForm.docDesc,
+              description4: ''
+            }
+          )
+        }
+        this.sectionWordsDialog.visible = false
+      })
+    },
     handleCommand(command) {
       // this.$message('click on item ' + command)
-      if (command === 'keShi') {
+      if (command === 'section') {
         this.tag = '科室管理'
         this.column1 = '科室ID'
         this.column2 = '科室名'
@@ -137,7 +322,7 @@ export default {
             }
           ]
         })
-      } else if (command === 'yaoPin') {
+      } else if (command === 'medicine') {
         this.tag = '药品管理'
         this.column1 = '药品ID'
         this.column2 = '药品名'
@@ -172,7 +357,7 @@ export default {
             }
           ]
         })
-      } else if (command === 'shouFei') {
+      } else if (command === 'fee') {
         this.tag = '收费管理'
         this.column1 = '收费项目ID'
         this.column2 = '收费项目名'
@@ -208,7 +393,7 @@ export default {
             }
           ]
         })
-      } else if (command === 'huaYan') {
+      } else if (command === 'examination') {
         this.tag = '化验项目管理'
         this.column1 = '化验项目ID'
         this.column2 = '化验项目名'
@@ -244,7 +429,7 @@ export default {
             }
           ]
         })
-      } else if (command === 'yiMiao') {
+      } else if (command === 'vaccine') {
         this.tag = '疫苗管理'
         this.column1 = '疫苗ID'
         this.column2 = '疫苗名'
@@ -280,7 +465,7 @@ export default {
             }
           ]
         })
-      } else if (command === 'zhuYuan') {
+      } else if (command === 'hospitalize') {
         this.tag = '住院管理'
         this.column1 = 'ID'
         this.column2 = '动物名字'

48a5b893471e5013dd7f5cee33b0081be76e40bc
whitfield-lyx <10842089@qq.com>
2021-03-24 16:47:27 +0800

    update: 用户管理模块 增加添加用户按钮 修改样式

--- src/views/manage-user/index.vue
+++ src/views/manage-user/index.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="app-container">
+    <el-button type="primary" style="margin-bottom: 10px;" @click="onCreateNewClicked">新建<i
+      class="el-icon-plus el-icon--right"
+    /></el-button>
     <el-table
       v-loading="listLoading"
       :data="list"
@@ -25,98 +28,148 @@
       </el-table-column>
       <el-table-column label="Password" width="200" align="center">
         <template slot-scope="scope">
-          <el-input v-show="scope.row.edit" size="small" v-model="scope.row.password"></el-input>
-          <span v-show="!scope.row.edit">{{ scope.row.password }}</span>
+          <span>{{ scope.row.password }}</span>
         </template>
       </el-table-column>
-      <el-table-column align="center" label="Actions" width="400">
-        <template slot-scope="{row}">
-          <el-button v-show='!row.edit' type="primary" @click='cancelEdit(row);row.edit=true;' icon="edit">修改密码</el-button>
-          <el-button v-show='row.edit' type="success" @click='confirmEdit(row);row.edit=false;' icon="check">修改完成</el-button>
-          <el-button type="danger" @click="deleteUser">删除账户</el-button>
+      <el-table-column label="Type" width="200" align="center">
+        <template slot-scope="scope">
+          <span>{{ scope.row.type }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column align="center" prop="created_at" label="Action" width="200">
+        <template slot-scope="{ row }">
+          <el-button-group>
+            <el-button type="primary" icon="el-icon-edit" @click="onEditClicked(row)"/>
+            <el-button type="danger" icon="el-icon-delete" @click="onDeleteClicked(row)"/>
+          </el-button-group>
         </template>
       </el-table-column>
     </el-table>
+
+    <el-dialog
+      :visible="editDialog.visible"
+      :title="editDialog.title"
+      width="50%"
+      center
+    >
+      <el-form :model="form">
+        <el-form-item label="账号" label-width="120px">
+          <el-input v-model="form.caseName" autocomplete="off"/>
+        </el-form-item>
+        <el-form-item label="用户名" label-width="120px">
+          <el-input v-model="form.jieZhen" autocomplete="off"/>
+        </el-form-item>
+        <el-form-item label="密码" label-width="120px">
+          <el-input v-model="form.zhenDuan" autocomplete="off"/>
+        </el-form-item>
+        <el-form-item label="账号类型" label-width="120px">
+          <el-input v-model="form.zhiLiao" autocomplete="off"/>
+        </el-form-item>
+      </el-form>
+      <template #footer>
+        <span class="dialog-footer">
+          <el-button @click="editDialog.visible = false">取 消</el-button>
+          <el-button type="primary" @click="editDialogConfirmOnClicked">确 定</el-button>
+        </span>
+      </template>
+    </el-dialog>
+
   </div>
 </template>
 
 <script>
-import { getUserList } from '@/api/user'
+import {getList} from "../../api/table";
 
 export default {
   data() {
     return {
-      list: [{
-        index: '0',
-        name: '我是超级账号！',
-        account: 'admin',
-        password: '111111',
-        edit: true
-      }],
-      listLoading: false,
+      list: null,
+      listLoading: true,
       listQuery: {
         page: 1,
         limit: 10
-      }
-    }
-  },
-  methods: {
-    cancelEdit(row) {
-      row.title = row.originalTitle
-      row.edit = false
-      this.$message({
-        message: 'The title has been restored to the original value',
-        type: 'warning'
-      })
-    },
-    confirmEdit(row) {
-      row.edit = false
-      row.originalTitle = row.title
-      this.$message({
-        message: 'The title has been edited',
-        type: 'success'
-      })
-    },
-    deleteUser(){
-      this.$confirm('此操作将永久删除该账户, 是否继续?', '提示', {
-        confirmButtonText: '确定',
-        cancelButtonText: '取消',
-        type: 'warning'
-      }).then(() => {
-        this.$message({
-          type: 'success',
-          message: '删除成功!'
-        });
-      }).catch(() => {
-        this.$message({
-          type: 'info',
-          message: '已取消删除'
-        });
-      });
-    }
-  }
-  /* created() {
-    this.fetchData()
-  },
-  methods: {
-  async getList() {
-        this.listLoading = true
-        const { data } = await fetchList(this.listQuery)
-        const items = data.items
-        this.list = items.map(v => {
-          this.$set(v, 'edit', false) //
-          v.originalTitle = v.title //  will be used when user click the cancel botton
-          return v
-        })
-        this.listLoading = false
       },
-    fetchData() {
-      this.listLoading = true
-      getUserList().then(response => {
-        this.list = response.data.items
-        this.listLoading = false
-      })
+      editDialog: {
+          visible: false,
+          contentVisible: true,
+          title: ''
+      },
+      form: {
+          userId: -1,
+          name: '',
+          account: '',
+          password: '',
+          type: ''
+      },
     }
-  }*/
+  },
+    created() {
+        this.fetchData()
+    },
+  methods: {
+    fetchData() {
+        this.listLoading = false
+        getList().then(response => {
+            const data = [{
+                userId: 1,
+                name: 'LYX',
+                account: 'admin123',
+                password: '111111',
+                type: 'admin',
+            }, {
+                userId: 2,
+                name: 'GXW',
+                account: 'admin456',
+                password: '222222',
+                type: 'admin',
+            },{
+                userId: 3,
+                name: 'YFF',
+                account: 'user123',
+                password: '333333',
+                type: 'user',
+            }]
+            this.list = data
+            this.listLoading = false
+        })
+    },
+      onCreateNewClicked() {
+          this.editDialog.visible = true
+          this.editDialog.title = '新建账户'
+          this.form.name = ''
+          this.form.account = ''
+          this.form.password = ''
+          this.form.type = ''
+          this.editDialog.changeMode = 'add'
+      },
+      onEditClicked(row) {
+          this.form.name = row.name
+          this.form.account = row.account
+          this.form.password = row.password
+          this.form.type = row.type
+          this.editDialog.title = '修改账户'
+          this.editDialog.visible = true
+          this.editDialog.title = '新建账户'
+          this.editDialog.changeMode = 'update'
+      },
+  // async getList() {
+  //       this.listLoading = true
+  //       const { data } = await fetchList(this.listQuery)
+  //       const items = data.items
+  //       this.list = items.map(v => {
+  //         this.$set(v, 'edit', false) //
+  //         v.originalTitle = v.title //  will be used when user click the cancel botton
+  //         return v
+  //       })
+  //       this.listLoading = false
+  //     },
+  //   fetchData() {
+  //     this.listLoading = true
+  //     getUserList().then(response => {
+  //       this.list = response.data.items
+  //       this.listLoading = false
+  //     })
+  //   }
+  }
 }
 </script>

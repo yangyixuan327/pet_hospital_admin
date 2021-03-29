f1beb55ea20ddd2d01746b65d3f418f483d9f0a6
whitfield-lyx <10842089@qq.com>
2021-03-29 09:17:22 +0800

    update: init user api

--- src/api/user.js
+++ src/api/user.js
@@ -1,13 +1,80 @@
 import request from '@/utils/request'
 
-export function getUserList(params) {
+export function getUserList() {
   return request({
-    url: '/user',
-    method: 'get',
-    params
+    url: '/admin/user/getNormal',
+    method: 'get'
+
   })
 }
+export function getAdminList() {
+  return request({
+    url: '/admin/user/getAdmins',
+    method: 'get'
 
+  })
+}
+export function getUserList() {
+  return request({
+    url: '/admin/user/getAll',
+    method: 'get'
+  })
+}
+export function getUser(data) {
+  return request({
+    url: '/admin/user/getById',
+    method: 'get',
+    data //userId
+  })
+}
+export function newUser(data) {
+  return request({
+    url: '/admin/user/newUser',
+    method: 'put',
+    data //user
+  })
+}
+export function updateUser(data) {
+  return request({
+    url: '/admin/user/updateInfo',
+    method: 'post',
+    data // userId用于验证用户是否存在
+  })
+}
+export function deleteUser(data) {
+  return request({
+    url: '/admin/user/delete',
+    method: 'delete',
+    data // userId
+  })
+}
+export function changeRole(data) {
+  return request({
+    url: '/admin/user/updateInfo',
+    method: 'post',
+    data // userId + role
+  })
+}
+export function submitEditDialogResult(params) {
+  console.log(params)
+  if (params.changeMode != null) {
+    if (params.changeMode === 'add') {
+      // 添加账户
+      return request({
+        url: '/vue-admin-template/table/list',
+        method: 'get',
+        params
+      })
+    } else {
+      // 修改账户
+      return request({
+        url: '/vue-admin-template/table/list',
+        method: 'get',
+        params
+      })
+    }
+  }
+}
 export function login(data) {
   return request({
     url: '/vue-admin-template/user/login',

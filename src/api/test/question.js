ac41038b671b2b641dc23dfe804211f8743a7fae
yuanfeifan <1272091180@qq.com>
2021-04-01 21:08:06 +0800

    update: 测试管理模块试题页面前后端对接成功

--- src/api/test/question.js
+++ src/api/test/question.js
@@ -1,19 +1,38 @@
 import request from '@/utils/request'
+import request_admin from "@/utils/request_admin";
 
-export function deleteQuestionById(question_id) {
-  const params = {
-    'Question_id': question_id
-  }
-  // todo 修改api
-  return request({
-    url: '/vue-admin-template/table/list',
+export function getQuestionList() {
+  return request_admin({
+    url: '/admin/test/question',
     method: 'get',
-    params
+    params: null
+  })
+}
+
+export function changeQuestionById(question_id, question) {
+  return request_admin({
+    url: '/admin/test/question/' + question_id,
+    method: 'post',
+    data: question
+  })
+}
+
+export function addQuestion(question) {
+  return request_admin({
+    url: 'admin/test/question',
+    method: 'put',
+    data: question
+  })
+}
+export function deleteQuestionById(question_id) {
+  return request_admin({
+    url: '/admin/test/question',
+    method: 'delete',
+    params: question_id
   })
 }
 
 export function submitWordsDialogResult(params) {
-  console.log(params)
   if (params.changeMode != null) {
     if (params.changeMode === 'add') {
       // 添加病例

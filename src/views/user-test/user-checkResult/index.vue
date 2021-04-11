f9eaa1af5c9c580c6052cb71a52fea20a1a1b61e
yuanfeifan <1272091180@qq.com>
2021-04-11 22:39:33 +0800

    update: 初步完成前台考试模块参加考试以及查看结果界面

--- src/views/user-test/user-checkResult/index.vue
+++ src/views/user-test/user-checkResult/index.vue
@@ -1,10 +1,99 @@
 <template>
+  <div class="app-container">
+    <el-table
+      v-loading="listLoading"
+      :data="list"
+      element-loading-text="Loading"
+      border
+      fit
+      highlight-current-row
+    >
+      <el-table-column align="center" label="考试ID" width="95">
+        <template slot-scope="scope">
+          {{ scope.$index }}
+        </template>
+      </el-table-column>
+      <el-table-column label="考试名称">
+        <template slot-scope="scope">
+          {{ scope.row.title }}
+        </template>
+      </el-table-column>
+      <el-table-column label="开始时间" width="110" align="center">
+        <template slot-scope="scope">
+          <span>{{ scope.row.pageviews }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="时长" width="110" align="center">
+        <template slot-scope="scope">
+          {{ scope.row.pageviews }}
+        </template>
+      </el-table-column>
+      <el-table-column label="总分" width="110" align="center">
+        <template slot-scope="scope">
+          {{ scope.row.pageviews }}
+        </template>
+      </el-table-column>
 
+      <el-table-column align="center" prop="created_at" label="操作" width="200">
+        <template slot-scope="scope">
+          <el-button-group>
+            <el-button type="primary" icon="el-icon-tickets" @click="onclick(scope.$index, scope.$index)"/>
+          </el-button-group>
+        </template>
+      </el-table-column>
+    </el-table>
+  </div>
 </template>
 
 <script>
+
+import { getList } from '@/api/table'
+import router from "@/router";
+
 export default {
-  name: "index"
+  data() {
+    return {
+      list: null,
+      listLoading: true,
+      wordsDialog: {
+        visible: false,
+        title: '',
+        changeMode: 'add' // has two value: 'add' and 'update'
+      },
+      form: {
+        examName: '',
+        startTime: null,
+        duration: 0,
+        totalScore: 0
+      }
+    }
+  },
+  created() {
+    this.fetchData()
+  },
+  methods: {
+    fetchData() {
+      this.listLoading = true
+      getList().then(response => {
+        this.list = response.data.items
+        this.listLoading = false
+      })
+    },
+    onclick(exam_id, exam_index) {
+      this.$router.push({
+        path: 'yourPath',
+        name: '要跳转的路径的 name,在 router 文件夹下的 index.js 文件内找',
+        params: {
+          key: 'key',
+          msgKey: this.msg
+        }
+        /*query: {
+            key: 'key',
+            msgKey: this.msg
+        }*/
+      })
+    }
+  }
 }
 </script>
 

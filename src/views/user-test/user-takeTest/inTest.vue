16c9b3180825d1c65b4cbee230902b218e940d1d
yuanfeifan <1272091180@qq.com>
2021-04-12 08:10:21 +0800

    fix: 跳转页面无法显示序号

--- src/views/user-test/user-takeTest/inTest.vue
+++ src/views/user-test/user-takeTest/inTest.vue
@@ -4,21 +4,21 @@
       <el-row>
         <el-col>
           <div class="grid-content bg-purple">
-            <div v-for="(test, index) in tests" :key="inTest">
-              <p>{{ inTest + 1 }}.{{ test.title }}</p>
+            <div v-for="(test, index) in tests" :key="index">
+              <p>{{ index + 1 }}.{{ test.title }}</p>
               <el-radio-group v-if="test.type == '选择题'" v-model="test.answer">
                 <!-- label绑定答案的值,可以绑定索引index,也可以绑定答案内容option -->
                 <el-radio
                   v-for="(option,index) in choiceOption"
-                  :key="inTest"
-                  :label="inTest"
+                  :key="index"
+                  :label="index"
                 >{{ option }}</el-radio>
               </el-radio-group>
               <el-radio-group v-else-if="test.type == '判断题'" v-model="test.answer">
                 <el-radio
                   v-for="(option,index) in judgeOption"
-                  :key="inTest"
-                  :label="inTest"
+                  :key="index"
+                  :label="index"
                 >{{ option }}</el-radio>
               </el-radio-group>
               <div v-else-if="test.type == '简答题'">

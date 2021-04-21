7e29c3e1d65248edb4b5ccab6e3e7212150f6f9f
makise <10175101106@stu.ecnu.edu.cn>
2021-04-21 20:33:14 +0800

    feat: format code and add iframe source

--- src/views/hospital_navigation/index.vue
+++ src/views/hospital_navigation/index.vue
@@ -3,13 +3,13 @@
     <el-container>
       <el-container>
         <el-aside width="1280px" height="728px">
-<!--          <iframe width="1024px" height="728px" allowfullscreen frameborder="0" src="https://orbix360.com/GvdUYnQ35"></iframe>-->
+          <iframe width="1280px" height="728px" allowfullscreen frameborder="0" src="https://orbix360.com/KkS4bhWHw"></iframe>
         </el-aside>
-        <el-main >
+        <el-main>
           <template>
             <div>
               <el-divider content-position="left">职能业务描述：</el-divider>
-              <span>{{description_by_characters}}</span>
+              <span>{{ description_by_characters }}</span>
               <el-divider></el-divider>
               <span></span>
             </div>
@@ -17,7 +17,7 @@
         </el-main>
       </el-container>
       <el-footer>
-        <el-row type="flex" :gutter="20" justify="end" >
+        <el-row type="flex" :gutter="20" justify="end">
           <el-col :span="3">
             <el-select v-model="character" @change=characterChange placeholder="请选择角色" size="medium">
               <el-option
@@ -47,120 +47,114 @@
 </template>
 
 <script>
-    import {getSectionInfo, getSectionsInfo} from "../../api/3D_navigation";
+import { getSectionInfo, getSectionsInfo } from '../../api/3D_navigation'
+
 export default {
-    data() {
-        return {
-            description_by_characters:"青春是一个短暂的美梦, 当你醒来时, 它早已消失无踪",
-            characters: [{
-                character_value: 'rec',
-                character_label: '前台'
-            }, {
-                character_value: 'doc',
-                character_label: '医师'
-            }, {
-                character_value: 'assis',
-                character_label: '医师助理'
-            }],
-            character:"前台",
-            sections: [],
-            section:"大厅", //默认大厅
-            sectionId: 1
-        }
-    },
-    created() {
-        this.fetchData()
-    },
-    methods: {
-        characterChange(val) {
-            let obj = {}
-            obj = this.characters.find((character) => {
-                return character.character_value === val
-            })
-            this.character = obj.character_value
-
-            let obj2 = {}
-            obj2 = this.sections.find((section) => {
-                return section.sectionId === this.sectionId
-            })
-
-            let description;
-            if (this.character === "rec") {
-                description = obj2.recDescrip
-            } else if (this.character === "doc") {
-                description = obj2.docDescrip
-            } else if (this.character === "assis") {
-                description = obj2.assisDescrip
-            } else{
-                description = obj2.recDescrip //默认前台
-            }
-            this.description_by_characters = description
-            this.$forceUpdate();
-        },
-        sectionChange(val) {
-            let obj = {}
-            obj = this.sections.find((section) => {
-                return section.sectionId === val
-            })
-            this.section = obj.sectionName
-            this.sectionId = obj.sectionId
-            let description;
-            if (this.character === "rec") {
-                description = obj.recDescrip
-            } else if (this.character === "doc") {
-                description = obj.docDescrip
-            } else if (this.character === "assis") {
-                description = obj.assisDescrip
-            } else{
-                description = obj.recDescrip //默认前台
-            }
-            this.description_by_characters = description
-            this.$forceUpdate();
-        },
-        fetchData() {
-            getSectionsInfo().then(response => {
-                this.sections = response.data.responseMap.result
-                console.log(this.sections)
-            })
-            this.description_by_characters = "请选择角色或者地点进行职能学习"
-        }
+  data() {
+    return {
+      description_by_characters: '青春是一个短暂的美梦, 当你醒来时, 它早已消失无踪',
+      characters: [{
+        character_value: 'rec',
+        character_label: '前台'
+      }, {
+        character_value: 'doc',
+        character_label: '医师'
+      }, {
+        character_value: 'assis',
+        character_label: '医师助理'
+      }],
+      character: '前台',
+      sections: [],
+      section: '大厅', // 默认大厅
+      sectionId: 1
     }
+  },
+  created() {
+    this.fetchData()
+  },
+  methods: {
+    characterChange(val) {
+      let obj = {}
+      obj = this.characters.find((character) => {
+        return character.character_value === val
+      })
+      this.character = obj.character_value
+
+      let obj2 = {}
+      obj2 = this.sections.find((section) => {
+        return section.sectionId === this.sectionId
+      })
+
+      let description
+      if (this.character === 'rec') {
+        description = obj2.recDescrip
+      } else if (this.character === 'doc') {
+        description = obj2.docDescrip
+      } else if (this.character === 'assis') {
+        description = obj2.assisDescrip
+      } else {
+        description = obj2.recDescrip // 默认前台
+      }
+      this.description_by_characters = description
+      this.$forceUpdate()
+    },
+    sectionChange(val) {
+      let obj = {}
+      obj = this.sections.find((section) => {
+        return section.sectionId === val
+      })
+      this.section = obj.sectionName
+      this.sectionId = obj.sectionId
+      let description
+      if (this.character === 'rec') {
+        description = obj.recDescrip
+      } else if (this.character === 'doc') {
+        description = obj.docDescrip
+      } else if (this.character === 'assis') {
+        description = obj.assisDescrip
+      } else {
+        description = obj.recDescrip // 默认前台
+      }
+      this.description_by_characters = description
+      this.$forceUpdate()
+    },
+    fetchData() {
+      getSectionsInfo().then(response => {
+        this.sections = response.data.responseMap.result
+        console.log(this.sections)
+      })
+      this.description_by_characters = '请选择角色或者地点进行职能学习'
+    }
+  }
 }
 </script>
 
 <style scoped>
-  .el-header, .el-footer {
-    background-color: #B3C0D1;
-    color: #333;
-    text-align: center;
-    line-height: 60px;
-  }
+.el-header, .el-footer {
+  background-color: #B3C0D1;
+  color: #333;
+  text-align: center;
+  line-height: 60px;
+}
 
-  .el-aside {
-    background-color: #D3DCE6;
-    color: #333;
-    text-align: center;
-    line-height: 200px;
-  }
+.el-main {
+  /*background-color: #E9EEF3;*/
+  color: #333;
+  text-align: center;
+  line-height: 160px;
+}
 
-  .el-main {
-    /*background-color: #E9EEF3;*/
-    color: #333;
-    text-align: center;
-    line-height: 160px;
-  }
+body > .el-container {
+  margin-bottom: 40px;
+}
 
-  body > .el-container {
-    margin-bottom: 40px;
-  }
+.el-container:nth-child(5) .el-aside,
+.el-container:nth-child(6) .el-aside {
+  line-height: 260px;
+}
 
-  .el-container:nth-child(5) .el-aside,
-  .el-container:nth-child(6) .el-aside {
-    line-height: 260px;
-  }
-
-  .el-container:nth-child(7) .el-aside {
-    line-height: 320px;
-  }
+.el-container:nth-child(7) .el-aside {
+  line-height: 320px;
+}
 </style>
 

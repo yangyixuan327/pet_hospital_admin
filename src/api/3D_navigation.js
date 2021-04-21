f943942174f9575ce5ac29a78f8d5018ec1b21e7
whitfield-lyx <10842089@qq.com>
2021-04-21 18:40:20 +0800

    update: api update

--- src/api/3D_navigation.js
+++ src/api/3D_navigation.js
@@ -1,4 +1,3 @@
-import request from '@/utils/request'
 import request_admin from '@/utils/request_admin'
 
 export function getSectionsInfo() {
@@ -14,39 +13,7 @@ export function getSectionInfo(sectionId) {
   })
 }
 
-export function getStructureInfo(command) {
-  if (command === 'section') {
-    return request_admin({
-      url: '/admin/structure/section',
-      method: 'get'
-    })
-  } else if (command === 'medicine') {
-    return request_admin({
-      url: '/admin/structure/medicine',
-      method: 'get'
-    })
-  } else if (command === 'fee') {
-    return request_admin({
-      url: '/admin/structure/fee',
-      method: 'get'
-    })
-  } else if (command === 'examination') {
-    return request_admin({
-      url: '/admin/structure/examination',
-      method: 'get'
-    })
-  } else if (command === 'vaccine') {
-    return request_admin({
-      url: '/admin/structure/vaccine',
-      method: 'get'
-    })
-  } else if (command === 'hospitalize') {
-    return request_admin({
-      url: '/admin/structure/hospitalize',
-      method: 'get'
-    })
-  }
-}
+
 
 
 

c1587b547e33713029e415c427cf70bf6e4cc601
yuanfeifan <1272091180@qq.com>
2021-04-26 09:24:18 +0800

    Update: 管理员考试页面挑选用户页面

--- src/api/test/exam.js
+++ src/api/test/exam.js
@@ -1,4 +1,43 @@
 import request from '@/utils/request'
+import request_admin from "@/utils/request_admin";
+export function getList() {
+  return request_admin({
+    url: '/admin/test/paper',
+    method: 'get',
+    params: null
+  })
+}
+
+export function getAllUser(testOptionId) {
+  return request_admin({
+    url: '/admin/test/paper/' + testOptionId + '/parUsers',
+    method: 'get',
+  })
+}
+
+export function changePaperById(paper_id, paper) {
+  return request_admin({
+    url: '/admin/test/paper/' + paper_id,
+    method: 'post',
+    data: paper
+  })
+}
+
+export function addPaper(paper) {
+  return request_admin({
+    url: 'admin/test/paper',
+    method: 'put',
+    data: paper
+  })
+}
+
+export function deletePaperById(paper_id) {
+  return request_admin({
+    url: '/admin/test/paper',
+    method: 'delete',
+    params: paper_id
+  })
+}
 
 export function deleteExamById(exam_id) {
   const params = {

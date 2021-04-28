43a81a09820a9ccb7e02aaaa8b0bc3515c6f755c
makise <10175101106@stu.ecnu.edu.cn>
2021-04-28 13:05:11 +0800

    fix: fix image refresh after upload successfully

--- src/views/manage-case/index.vue
+++ src/views/manage-case/index.vue
@@ -425,11 +425,13 @@ export default {
       this.$message.warning(`当前限制选择 3 个文件，本次选择了 ${files.length} 个文件，共选择了 ${files.length + fileList.length} 个文件`)
     },
     handleUploadSuccess(res, file) {
-      var videoUrl = res.responseMap.result
-      if (videoUrl !== null) {
-        this.videoUrl = 'http://' + videoUrl
+      var url = res.responseMap.result
+      if (url !== null) {
+        this.videoUrl = 'http://' + url
+        this.imageUrls = ['http://' + url]
       } else {
         this.videoUrl = ''
+        this.imageUrls = []
       }
     },
     handleBeforeUpload(file) {
